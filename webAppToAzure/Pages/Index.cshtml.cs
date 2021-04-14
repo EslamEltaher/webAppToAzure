d27598bce8f0a4c928c4a53c51e226bada6ee5fd
eslameltaher <eslam.eltaher@hotmail.com>
2021-04-14 08:19:27 +0200

    Add project files.

---
+++ webAppToAzure/Pages/Index.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace webAppToAzure.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly myDbContext context;
+        public List<City> Cities { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger, myDbContext context)
+        {
+            _logger = logger;
+            this.context = context;
+        }
+
+        public void OnGet()
+        {
+            Cities = context.Cities.ToList();
+        }
+    }
+}
---
+++ webAppToAzure/myDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace webAppToAzure
+{
+    public class myDbContext : DbContext
+    {
+        public myDbContext(DbContextOptions<myDbContext> options) : base(options) { }
+        public DbSet<City> Cities { get; set; }
+    }
+}
